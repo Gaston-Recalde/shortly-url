3bd33866482b51f24335831a6b7690d0e8354b47
Gaston Recalde <gastonrg94@gmail.com>
2023-03-08 07:56:24 -0300

    Nav responsive mobile terminado

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 const btnOpen = document.querySelector('.open');
 const btnClose = document.querySelector('.close');
+const navbar = document.querySelector('.nav__container__items');
 
 btnOpen.addEventListener('click' , () => {
     btnOpen.style.display = 'none';
     btnClose.style.display = 'inline-block';
+    navbar.style.display = 'block';
 });
 
 btnClose.addEventListener('click' , () => {
     btnOpen.style.display = 'inline-block';
     btnClose.style.display = 'none';
+    navbar.style.display = 'none';
 });
