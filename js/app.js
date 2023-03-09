aed13ef4f54b1cdb06d81639e61cdda8660d0e3c
Gaston Recalde <gastonrg94@gmail.com>
2023-03-09 08:13:11 -0300

    Correcion de errores y ajustes

--- js/app.js
+++ js/app.js
@@ -2,11 +2,28 @@ const btnOpen = document.querySelector('.open');
 const btnClose = document.querySelector('.close');
 const navbar = document.querySelector('.nav__container__items');
 const form = document.querySelector('form');
+const input = document.querySelector('input');
+const button = document.querySelector('.form__button');
+const small = document.querySelector('small');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
+    checkInput();
 });
+
+function checkInput() {
+
+    const linkValue = input.value.trim();
+
+    if(linkValue === '') {
+        input.classList.add('error')
+        small.style.display = 'inline-block'
+      } else {
+        input.classList.remove('error')
+        small.style.display = 'none'
+      }
+};
 
 btnOpen.addEventListener('click' , () => {
     btnOpen.style.display = 'none';
